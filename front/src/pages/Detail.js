0441f24cb9c071f6e57a81c3272101fb9d689570
sungeun101 <josungeun1019@gmail.com>
2021-08-17 18:17:18 +0900

    feat : Add heart click

--- front/src/pages/Detail.js
+++ front/src/pages/Detail.js
@@ -1,9 +1,54 @@
 import React, { useState } from 'react';
 import Gallery from '../components/Gallery';
+import { Divider } from 'antd';
+import {
+  HeartIcon,
+  HeartCount,
+  Name,
+  NameContainer,
+  StarContainer,
+  TitleContainer,
+  StarIcon,
+} from './Detail.style';
+import { faStar, faHeart } from '@fortawesome/free-solid-svg-icons';
+import {
+  faStar as farStar,
+  faHeart as farHeart,
+} from '@fortawesome/free-regular-svg-icons';
+
 const Detail = () => {
+  const [liked, setLiked] = useState(true);
+  const star = Array.from({ length: 3 });
+  const emptyStar = Array.from({ length: 5 - star.length });
+
+  const handleLikes = () => {
+    setLiked((prev) => !prev);
+  };
+
   return (
     <div>
       <Gallery />
+
+      <TitleContainer>
+        <NameContainer>
+          <Name>ABC카페</Name>
+          <HeartIcon
+            onClick={handleLikes}
+            icon={liked ? faHeart : farHeart}
+          ></HeartIcon>
+          <HeartCount>120</HeartCount>
+        </NameContainer>
+        <StarContainer>
+          {star.map((count, i) => {
+            return <StarIcon key={i} icon={faStar} size="2x" />;
+          })}
+          {emptyStar.map((count, i) => {
+            return <StarIcon key={i} icon={farStar} size="2x" />;
+          })}
+        </StarContainer>
+      </TitleContainer>
+
+      <Divider />
     </div>
   );
 };
