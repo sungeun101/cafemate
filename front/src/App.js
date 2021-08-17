5da186b530be707fddd01d480be3ae43a4f0d686
sungeun101 <josungeun1019@gmail.com>
2021-08-17 18:01:44 +0900

    feat : Add image slider

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import { BrowserRouter } from "react-router-dom"
-import Main from './pages/Main'
+import { BrowserRouter } from 'react-router-dom';
+import Main from './pages/Main';
+// import React from 'react';
+// import 'antd/dist/antd.css';
+// import GlobalStyle, { Container } from './globalStyles';
+// import Detail from './pages/Detail';
 
 function App() {
   return (
     <BrowserRouter>
       <Main />
     </BrowserRouter>
+
+    // function App() {
+    //   return (
+    //     <>
+    //       <GlobalStyle />
+    //       <Container>
+    //         <div className="App">
+    //           <Detail />
+    //         </div>
+    //       </Container>
+    //     </>
   );
 }
 
