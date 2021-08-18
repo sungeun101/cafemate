6d399d9226096d25f4f603201357fe4dfd4f04a5
sooyoungh <sooyoung.h8@gmail.com>
2021-08-18 19:47:15 +0900

    test : Edit testCase

--- back/src/test/java/cafemate/back/service/CommentsServiceTest.java
+++ back/src/test/java/cafemate/back/service/CommentsServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,51 +18,80 @@ import java.time.LocalDate;
 public class CommentsServiceTest {
 
     @Autowired CommentsRepository commentsRepository;
-    @Autowired CommentsService commentsService;
 
-    @Test //C 리뷰 쓰기 (테스트용 리턴)
-    @Rollback(value = false)
+    @Test //C 리뷰 쓰기
     public void 리뷰작성() throws Exception {
-        Comments commentFirst = new Comments();
-        Comments commentSecond = new Comments();
+        //given
+        String content = "댓글 내용";
+        String img_path = "이미지경로";
+        float star = 3.5F;
+        LocalDate created_at = LocalDate.now();
 
-        commentFirst.setContent("first review");
-        commentFirst.setCreated_at(LocalDate.now());
-        commentSecond.setContent("second review");
-        commentSecond.setCreated_at(LocalDate.now());
+        //when
+        Comments comment = commentsRepository.save(Comments.builder()
+                    .content(content)
+                    .img_path(img_path)
+                    .star(star)
+                    .created_at(created_at)
+                    .build());
 
-        Long saveIdFirst = commentsService.createComment(commentFirst); //저장하고 아이디 반환
-        Long saveIdSecond = commentsService.createComment(commentSecond);
-
-        assertEquals(commentFirst, commentsRepository.getById(saveIdFirst));
-        assertEquals(commentSecond, commentsRepository.getById(saveIdSecond));
-
+        //then
+        assertEquals(comment.getContent(), content);
+        assertEquals(comment.getImg_path(), img_path);
+        assertEquals(comment.getStar(), star,0.0001);
+        assertEquals(comment.getCreated_at(), created_at);
     }
 
     @Test
-    @Rollback(value = false)
     public void 리뷰수정() throws Exception {
-        Comments comment = commentsRepository.getById(1L);
-        System.out.println(comment.getId());
-        comment.setContent("change content");
+        //given
+        String originalContent = "이전 댓글 내용";
+        float originalStar = 3.5F;
+
+        String newContent = "바뀐 댓글 내용";
+        float newStar = 1F;
+
+        Comments comment = commentsRepository.save(Comments.builder()
+                .content(originalContent)
+                .star(originalStar)
+                .created_at(LocalDate.now())
+                .build());
+
+        //when
+        comment.updateContent(newContent,newStar);
+
+        //then
+        assertEquals(comment.getContent(), newContent);
+        assertEquals(comment.getStar(), newStar, 0.0001);
     }
 
     @Test
-    @Rollback(value = false)
     public void 리뷰삭제() throws Exception {
-        commentsRepository.deleteById(2L);
+        //given
+        String content = "댓글 내용";
+        float star = 3.5F;
+        Comments comment = commentsRepository.save(Comments.builder()
+                .content(content)
+                .star(star)
+                .build());
+        Integer commentId = comment.getId();
+
+        //when
+        commentsRepository.deleteById(commentId);
+        Comments deleteComment = commentsRepository.findById(commentId).orElse(null);
+
+        //then
+        assertNull(deleteComment);
     }
 
-    @Test
-    @Rollback(value = false)
-    public void 카페리뷰조회() throws Exception {
-
-    }
-
-    @Test
-    @Rollback(value = false)
-    public void 유저리뷰조회() throws Exception {
-
-    }
+//    @Test
+//    public void 카페리뷰조회() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void 유저리뷰조회() throws Exception {
+//
+//    }
     
 }
