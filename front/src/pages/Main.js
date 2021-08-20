45e6d02812a3869bdad89f69aba5d9fc39635d22
sungeun101 <josungeun1019@gmail.com>
2021-08-20 21:44:01 +0900

    feat : Add Router

---
+++ front/src/pages/Main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Main = () => {
+  let history = useHistory();
+  const cafe = {
+    id: 1,
+    name: '가로수 카페',
+    img_path: '이미지 url',
+    phone: '010-0000-0000',
+    time: '09:00~24:00',
+    star: 5,
+    menu: '아메리카노(HOT, ICE):3,500/카페민트:4,500',
+    area: '',
+    americano: '',
+    dessert: '',
+    parking: '',
+    wifi: '',
+    animal: '',
+  };
+
+  const onCafeClick = () => {
+    history.push({
+      pathname: `/detail/${cafe.id}`,
+      state: { cafe },
+    });
+  };
+
+  return (
+    <>
+      <h1>Main Page</h1>
+
+      <div onClick={onCafeClick}>cafe</div>
+    </>
+  );
+};
+
+export default Main;
