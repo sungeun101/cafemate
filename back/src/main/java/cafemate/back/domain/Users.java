450c86f632111531b50770afd36ff6679c62920a
sooyoungh <sooyoung.h8@gmail.com>
2021-08-20 20:14:44 +0900

    chore : Edit DTO,Entity

--- back/src/main/java/cafemate/back/domain/Users.java
+++ back/src/main/java/cafemate/back/domain/Users.java
@@ -31,9 +31,9 @@ public class Users {
 //    @OneToMany(mappedBy = "userLikes")
 //    private List<Likes> likes = new ArrayList<>();
 
-    @Builder
-    public Users(Integer id, String email, String name, String img_path) {
-        this.id = id;
+    @Builder //id는 자동생성이니까, 엔티티화할때 인자로 받을 필요없지만,
+    //테스트 케이스에서 id가 자동생성이 안되서 일단 id 항목도 인자로 받게 함
+    public Users(String email, String name, String img_path) {
         this.email = email;
         this.name = name;
         this.img_path = img_path;
--- back/src/main/java/cafemate/back/dto/users/UsersResponseDto.java
+++ back/src/main/java/cafemate/back/dto/users/UsersResponseDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class UsersResponseDto {
     private Integer id;
