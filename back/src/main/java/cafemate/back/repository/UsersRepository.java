59d7dc856f2bb637787f3e7874562ec461041478
zo uihyung <44282368+uihyung12@users.noreply.github.com>
2021-08-22 22:22:17 +0900

    Revert "prLikes"

--- back/src/main/java/cafemate/back/repository/UsersRepository.java
+++
@@ -1,8 +0,0 @@
-package cafemate.back.repository;
-
-import cafemate.back.domain.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users,Long> {
-    Users findUserByEmail(String email);
-}
