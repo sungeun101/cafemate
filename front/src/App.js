b9ca2820d52ccfc92c9362ca1ab6e8ceb32178bc
Stella Yun <stella.seryoon@gmail.com>
2021-08-23 22:14:21 +0900

    Merged front files

--- front/src/App.js
+++ front/src/App.js
@@ -1,47 +1,12 @@
-import React from 'react';
-import { Redirect, Route, BrowserRouter, Switch } from 'react-router-dom';
-import 'antd/dist/antd.css';
-import GlobalStyle, { Container } from './globalStyles';
-import Detail from './pages/Detail';
-import Main from './pages/Main';
-import My from './pages/My';
+import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import Main from './pages/Main'
 
 function App() {
   return (
-    <Container>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Main />
-          </Route>
-          <Route exact path="/detail/:id">
-            <Detail />
-          </Route>
-          <Route exact path="/my">
-            <My />
-          </Route>
-          <Route>
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Container>
-  );
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  )
 }
-
 export default App;
-
-// import './App.css';
-// import { BrowserRouter } from 'react-router-dom';
-// import Main from './pages/Main';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Main />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,100 +1,59 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import '../styles/Main.css';
+import React, { useState } from 'react'
+import { Route, Switch } from "react-router-dom"
+import Topbar from '../components/Topbar'
+import Searchbar from '../components/Searchbar'
+import Content from '../components/Content'
+import Detail from '../pages/Detail'
+import My from '../pages/My'
+import { Layout } from 'antd'
+import GlobalStyle, { Container } from '../globalStyles'
 
-const Main = () => {
-  let history = useHistory();
-  const cafe = {
-    id: 2,
-    name: '가로수 카페',
-    img_path: '이미지 url',
-    phone: '010-0000-0000',
-    time: '09:00~24:00',
-    star: 5,
-    menu: '아메리카노(HOT, ICE):3,500/카페민트:4,500',
-    area: '',
-    americano: '',
-    dessert: '',
-    parking: '',
-    wifi: '',
-    animal: '',
-  };
+const { Header, Sider } = Layout;
 
-  const onCafeClick = () => {
-    history.push({
-      pathname: `/detail/${cafe.id}`,
-      state: { cafe },
-    });
-  };
+function Main(){
+  const [googleId, setGoogleId ] = useState(null)
+
+  const [keyword, setKeyword] = useState("");
+  const [price, setPrice] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [address1, setAddress1] = useState(null);
+  const [address2, setAddress2] = useState(null);
+  const [address3, setAddress3] = useState(null);
+  const [tags, setTags] = useState([]);
+
+  const funcs = { setKeyword, setPrice, setRate, setAddress1, setAddress2, setAddress3, setTags }
+  const filterData = { keyword, price, rate, address1, address2, address3, tags }
 
   return (
-    <>
-      <h1>Main Page</h1>
-
-      <div onClick={onCafeClick}>cafe</div>
-    </>
-  );
-};
+    <Layout>
+    <Header>
+      <Topbar googleId={googleId} setGoogleId={setGoogleId} />
+    </Header>
+    <Switch>
+    <Route exact path="/detail/:id">
+      <Container>
+        <GlobalStyle />
+        <Detail />
+      </Container>
+    </Route>
+    <Route exact path="/my">
+      <Container>
+        <GlobalStyle />
+        <My />
+      </Container>
+    </Route>
+    <Route path="/" render={() =>
+        <Layout hasSider={true}>
+        <Sider>
+          <Searchbar funcs={funcs} filterData={filterData} />
+        </Sider>
+        <Content filterData={filterData} />
+      </Layout>
+    } />
+    </Switch>
+    </Layout>
+  )
+}
 
 export default Main;
-
-// import '../styles/Main.css';
-// import React, { useState } from 'react';
-// import { Route, Switch } from 'react-router-dom';
-// import Topbar from '../components/Topbar';
-// import Searchbar from '../components/Searchbar';
-// import MapContainer from '../components/MapContainer';
-// import Results from '../components/Results';
-// import { Layout } from 'antd';
-
-// const { Header, Sider } = Layout;
-
-// function Main() {
-//   const [keyword, setKeyword] = useState('');
-//   const [price, setPrice] = useState(0);
-//   const [rate, setRate] = useState(0);
-//   const [address1, setAddress1] = useState(null);
-//   const [address2, setAddress2] = useState(null);
-//   const [address3, setAddress3] = useState(null);
-//   const [tags, setTags] = useState([]);
-
-//   const funcs = {
-//     setKeyword,
-//     setPrice,
-//     setRate,
-//     setAddress1,
-//     setAddress2,
-//     setAddress3,
-//     setTags,
-//   };
-//   const filterData = {
-//     keyword,
-//     price,
-//     rate,
-//     address1,
-//     address2,
-//     address3,
-//     tags,
-//   };
-
-//   return (
-//     <Layout>
-//       <Header>
-//         <Topbar />
-//       </Header>
-//       <Layout hasSider={true}>
-//         <Sider>
-//           <Searchbar funcs={funcs} filterData={filterData} />
-//         </Sider>
-//         <Switch>
-//           <Route
-//             path="/search"
-//             render={() => <Results filterData={filterData} />}
-//           />
-//           <Route path="/" render={() => <MapContainer />} />
-//         </Switch>
-//       </Layout>
-//     </Layout>
-//   );
-// }
-
-// export default Main;
