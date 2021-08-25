585294034999462b525688af5bd5f949d8430387
sungeun101 <josungeun1019@gmail.com>
2021-08-25 09:45:33 +0900

    chore : Check heartRef

--- front/src/App.js
+++ front/src/App.js
@@ -31,17 +31,3 @@ function App() {
 }
 
 export default App;
-
-// import './App.css';
-// import { BrowserRouter } from 'react-router-dom';
-// import Main from './pages/Main';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Main />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
--- front/src/pages/My/Cafes.js
+++ front/src/pages/My/Cafes.js
@@ -1,5 +1,5 @@
 import { Empty, Skeleton, Card } from 'antd';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { cafeService } from 'service/cafes';
 import { likesService } from 'service/likes';
@@ -22,6 +22,8 @@ const MyCafes = () => {
 
   let history = useHistory();
 
+  const heartRef = useRef();
+
   useEffect(() => {
     getLikes();
   }, []);
@@ -38,7 +40,7 @@ const MyCafes = () => {
     setLoading(true);
     try {
       const res = await likesService.getLikedByUserId(user_id);
-      console.log('getLikes result : ', res.data);
+      // console.log('getLikes result : ', res.data);
       setLikes(res.data);
     } catch (e) {
       console.log(e.messgae);
@@ -52,14 +54,18 @@ const MyCafes = () => {
     for await (const obj of likes) {
       // const res = await cafeService.getCafesById(obj.cafe_id);
       const res = await cafeService.getCafesById(obj.id);
-      console.log('getCafes result : ', res);
+      // console.log('getCafes result : ', res);
       resArr.push(res.data);
     }
     setMyCafes(resArr);
     setLoading(false);
   };
 
-  const onClick = (cafe) => {
+  const onClick = (cafe, e) => {
+    console.log('heartRef.current', heartRef.current);
+    console.log('e.target', e.target);
+    console.log(heartRef.current.contains(e.target));
+
     // history.push({
     //   pathname: `/detail/${cafe.id}`,
     //   state: { cafe },
@@ -67,17 +73,17 @@ const MyCafes = () => {
   };
 
   const RemoveFromLikes = async (cafe) => {
-    try {
-      const res = await likesService.cancelLike({
-        //   cafe_id: cafe.id,
-        id: cafe.id,
-        user_id,
-      });
-      console.log('cancelLike result : ', res);
-    } catch (e) {
-      console.log(e.message);
-    }
-    getLikes();
+    // try {
+    //   const res = await likesService.cancelLike({
+    //     //   cafe_id: cafe.id,
+    //     id: cafe.id,
+    //     user_id,
+    //   });
+    //   console.log('cancelLike result : ', res);
+    // } catch (e) {
+    //   console.log(e.message);
+    // }
+    // getLikes();
   };
 
   return loading ? (
@@ -92,7 +98,7 @@ const MyCafes = () => {
     <CafeContainer>
       {myCafes.map((cafe) => (
         <StyledCard
-          onClick={() => onClick(cafe)}
+          onClick={(e) => onClick(cafe, e)}
           key={cafe.id}
           hoverable
           cover={<Cover alt={cafe.name} src={cafe.img_path} />}
@@ -102,7 +108,7 @@ const MyCafes = () => {
             description={
               <Description>
                 <p>주소(동까지)</p>
-                <HeartContainer>
+                <HeartContainer ref={heartRef}>
                   <HeartIcon
                     icon={faHeart}
                     onClick={() => RemoveFromLikes(cafe)}
