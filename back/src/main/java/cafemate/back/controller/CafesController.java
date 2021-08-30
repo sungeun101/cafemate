eac9f246d9b4e455b7b2644a53a186e7cc451624
sooyoungh <sooyoung.h8@gmail.com>
2021-08-31 03:37:09 +0900

    feat : Add cafes paging

--- back/src/main/java/cafemate/back/controller/CafesController.java
+++ back/src/main/java/cafemate/back/controller/CafesController.java
@@ -1,13 +1,14 @@
 package cafemate.back.controller;
 
+import cafemate.back.domain.Likes;
 import cafemate.back.domain.Users;
 import cafemate.back.dto.cafes.CafesSearchResponseDto;
+import cafemate.back.dto.likes.LikesListDto;
 import cafemate.back.dto.users.UsersResponseDto;
 import cafemate.back.service.CafesService;
 import cafemate.back.service.LikesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,10 +28,19 @@ public class CafesController {
 
     // 메인페이지 검색, 다중 필터
     @GetMapping("/cafes")
-    public ResponseEntity<List<CafesSearchResponseDto>> getCafes(@RequestParam(value = "sorting") String sorting,
+    public ResponseEntity<Page<CafesSearchResponseDto>> getCafes(@RequestParam(value = "sorting") String sorting,
                                                                  @RequestParam(value = "dong") String dong,
-                                                                 @RequestParam(value = "filtering") String filtering) {
-        return new ResponseEntity<>(cafesService.getCafesList(sorting,dong, filtering ), HttpStatus.OK);
+                                                                 @RequestParam(value = "filtering") String filtering,
+                                                                 @RequestParam(value = "page") Integer page) {
+
+        List<CafesSearchResponseDto> cafesList = cafesService.getCafesList(sorting, dong, filtering);
+        Pageable pageable = PageRequest.of(page, 10, Sort.unsorted());
+
+        int start = (int) pageable.getOffset();
+        int end = (start + pageable.getPageSize() ) > cafesList.size() ? cafesList.size() : (start + pageable.getPageSize());
+        Page<CafesSearchResponseDto> cafesPage = new PageImpl<>(cafesList.subList(start,end), pageable, cafesList.size());
+
+        return new ResponseEntity<>( cafesPage ,HttpStatus.OK);
     }
 
 
