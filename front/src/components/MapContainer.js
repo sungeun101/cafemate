3c14826830076538577960b6ed1960917752128c
sungeun101 <josungeun1019@gmail.com>
2021-08-30 23:06:16 +0900

    feat : Show rate with real data

--- front/src/components/MapContainer.js
+++ front/src/components/MapContainer.js
@@ -1,39 +1,42 @@
 /* global kakao */
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-function MapContainer(){
-    const [width, setWidth] = useState(window.innerWidth);
+function MapContainer() {
+  const [width, setWidth] = useState(window.innerWidth);
 
-    useEffect(() => {
-        const container = document.getElementById("map");
-        const options = {
-            center: new kakao.maps.LatLng(33.361425, 126.529418),
-            level: 9
-        };
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(function(position){
-                var lat = position.coords.latitude, lon = position.coords.longitude;
-                var locPosition = new kakao.maps.LatLng(lat, lon);
-                map.setCenter(locPosition);
-            })
-        }
-        const handleResize = () => {
-            setWidth(window.innerWidth);
-        }
-        const map = new kakao.maps.Map(container, options);
-        window.addEventListener('resize', handleResize);
-        return() => {
-            window.removeEventListener('resize', handleResize);
-        }
-    }, [width])
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.361425, 126.529418),
+      level: 9,
+    };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude,
+          lon = position.coords.longitude;
+        var locPosition = new kakao.maps.LatLng(lat, lon);
+        map.setCenter(locPosition);
+      });
+    }
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    const map = new kakao.maps.Map(container, options);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width]);
 
-    return (
-        <div id="map" style={{
-            width: `${width - 200}px`,
-            height: '100vh'
-        }}>
-        </div>
-    )
+  return (
+    <div
+      id="map"
+      style={{
+        width: `${width - 200}px`,
+        height: '100vh',
+      }}
+    ></div>
+  );
 }
 
 export default MapContainer;
--- front/src/components/Stars.js
+++ front/src/components/Stars.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { StarContainer, StarIcon } from './Stars.style';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { StarContainer, StyledRate } from './Stars.style';
 
-const Stars = ({ star, size }) => {
-  const starArr = Array.from({ length: 5 }, (v, i) => i + 1);
-
+const Stars = ({ star }) => {
   return (
     <StarContainer>
-      {starArr.map((starNum) => (
-        <StarIcon
-          key={starNum}
-          icon={starArr.indexOf(starNum) < star ? faStar : farStar}
-          size={size || '2x'}
-        />
-      ))}
+      <StyledRate
+        allowHalf
+        disabled
+        defaultValue={Math.round(star * 100) / 100}
+      />
     </StarContainer>
   );
 };
